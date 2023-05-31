8c912115ac25e1bb4326bd64f2c1e3d612d22800
JcobV01 <jakubwadycki@gmail.com>
2023-05-31 22:38:20 +0200

    Finalny

--- monopoly.py
+++ monopoly.py
@@ -1,5 +1,8 @@
 import os
 import random
+import sys
+
+#klasa gracz, klasa plansza i ich obiekty
 
 class Player:
     def __init__(self, stan_konta, posiadlosci):
@@ -19,14 +22,10 @@ class Board:
         self.gracz = [g1, g2, g3, g4]
         self.wlasciciel = wlasciciel
 
-
-gracz1 = Player(2000000, [])
-gracz2 = Player(2000000, [])
-gracz3 = Player(2000000, [])
-gracz4 = Player(2000000, [])
-
-tabelaGraczy = [gracz1, gracz2, gracz3, gracz4]
-
+gracz1 = Player(700000, [])
+gracz2 = Player(700000, [])
+gracz3 = Player(700000, [])
+gracz4 = Player(700000, [])
 
 pole1 = Board(0, 'a', 'Start', 0, 'None', 0, 0, 'specjalne', 'O', 'X', '%', '&', 0)
 pole2 = Board(1, 'ą', 'Grenada', 60000, 'Hiszpania', 0, 3000, 'budynek', ' ', ' ', ' ', ' ', 0)
@@ -44,7 +43,7 @@ pole13 = Board(12, 'j', 'Szansa', 0, 'None', 0, 0, 'specjalne', ' ', ' ', ' ', '
 pole14 = Board(13, 'k', 'Hamburg', 180000, 'Niemcy', 0, 18000, 'budynek', ' ', ' ', ' ', ' ', 0)
 pole15 = Board(14, 'l', 'Cypr', 200000, 'Wyspa', 0, 20000, 'wyspa', ' ', ' ', ' ', ' ', 0)
 pole16 = Board(15, 'ł', 'Berlin', 200000, 'Niemcy', 0, 20000, 'budynek', ' ', ' ', ' ', ' ', 0)
-pole17 = Board(16, 'm', 'Mistrzostwa', 0, 'None', 0, 0, 'specjalne', ' ', ' ', ' ', ' ', 0)
+pole17 = Board(16, 'm', 'Impreza', 0, 'None', 0, 0, 'specjalne', ' ', ' ', ' ', ' ', 0)
 pole18 = Board(17, 'n', 'Londyn', 220000, 'WielkaBrytania', 0, 22000, 'budynek', ' ', ' ', ' ', ' ', 0)
 pole19 = Board(18, 'ń', 'Dubaj', 200000, 'Wyspa', 0, 20000, 'wyspa', ' ', ' ', ' ', ' ', 0)
 pole20 = Board(19, 'o', 'Sydnay', 240000, 'WielkaBrytania', 0, 24000, 'budynek', ' ', ' ', ' ', ' ', 0)
@@ -52,7 +51,7 @@ pole21 = Board(20, 'ó', 'Szansa', 0, 'None', 0, 0, 'specjalne', ' ', ' ', ' ', 
 pole22 = Board(21, 'p', 'Chicago', 260000, 'USA', 0, 26000, 'budynek', ' ', ' ', ' ', ' ', 0)
 pole23 = Board(22, 'q', 'LasVegas', 260000, 'USA', 0, 26000, 'budynek', ' ', ' ', ' ', ' ', 0)
 pole24 = Board(23, 'r', 'NowyJork', 280000, 'USA', 0, 28000, 'budynek', ' ', ' ', ' ', ' ', 0)
-pole25 = Board(24, 's', 'Podroz', 0, 'None', 0, 0, 'specjalne', ' ', ' ', ' ', ' ', 0)
+pole25 = Board(24, 's', 'Pole strzeżone', 0, 'None', 0, 0, 'specjalne', ' ', ' ', ' ', ' ', 0)
 pole26 = Board(25, 't', 'Nicea', 200000, 'Wyspa', 0, 20000, 'wyspa', ' ', ' ', ' ', ' ', 0)
 pole27 = Board(26, 'u', 'Lyon', 300000, 'Francja', 0, 30000, 'budynek', ' ', ' ', ' ', ' ', 0)
 pole28 = Board(27, 'v', 'Paryz', 320000, 'Francja', 0, 32000, 'budynek', ' ', ' ', ' ', ' ', 0)
@@ -61,23 +60,26 @@ pole30 = Board(29, 'x', 'Krakow', 350000, 'Polska', 0, 35000, 'budynek', ' ', ' 
 pole31 = Board(30, 'y', 'Podatek', 0, 'None', 0, 0, 'specjalne', ' ', ' ', ' ', ' ', 0)
 pole32 = Board(31, 'z', 'Warszawa', 400000, 'Polska', 0, 40000, 'budynek', ' ', ' ', ' ', ' ', 0)
 
-tablicaPol = [pole1, pole2, pole3, pole4, pole5, pole6, pole7, pole8, pole9, pole10, pole11, pole12, pole13, pole14, pole15, pole16, pole17, pole18, pole19, pole20, pole21, pole22, pole23, pole24, pole25, pole26, pole27, pole28, pole29, pole30, pole31]
+#tablice z obiektami żeby można było się po numerach odwoływać
 
+tablicaPol = [pole1, pole2, pole3, pole4, pole5, pole6, pole7, pole8, pole9, pole10, pole11, pole12, pole13, pole14, pole15, pole16, pole17, pole18, pole19, pole20, pole21, pole22, pole23, pole24, pole25, pole26, pole27, pole28, pole29, pole30, pole31]
+tabelaGraczy = [gracz1, gracz2, gracz3, gracz4]
+
+#tabele pomocnicze do rysowania planszy (boczne ściany)
 
 rightFilds = [pole10, pole11, pole12, pole13, pole14, pole15, pole16]
 leftFilds = [pole32, pole31, pole30, pole29, pole28, pole27, pole26]
 
+#aktualne pozycje graczy
 
 currentPlayerPosition = [0, 0, 0, 0]
 
 def setBoard():
-
+    #rysowanie planszy
     os.system('cls')
 
     leftLetters = ['z', 'y', 'x', 'w', 'v', 'u', 't']
     rightLetters = ['g', 'h', 'i', 'j', 'k', 'l', 'ł']
-
-    
 
     print(f"┌{'─'*91}┐")
     print(f"│ ┌───────┐ ┌───────┐ ┌───────┐ ┌───────┐ ┌───────┐ ┌───────┐ ┌───────┐ ┌───────┐ ┌───────┐ │")
@@ -99,14 +101,18 @@ def setBoard():
     print(f"│ │ {pole25.gracz[2]}   {pole25.gracz[3]} │ │ {pole24.gracz[2]}   {pole24.gracz[3]} │ │ {pole23.gracz[2]}   {pole23.gracz[3]} │ │ {pole22.gracz[2]}   {pole22.gracz[3]} │ │ {pole21.gracz[2]}   {pole21.gracz[3]} │ │ {pole20.gracz[2]}   {pole20.gracz[3]} │ │ {pole19.gracz[2]}   {pole19.gracz[3]} │ │ {pole18.gracz[2]}   {pole18.gracz[3]} │ │ {pole17.gracz[2]}   {pole17.gracz[3]} │ │")
     print(f"│ └───────┘ └───────┘ └───────┘ └───────┘ └───────┘ └───────┘ └───────┘ └───────┘ └───────┘ │")
     print(f"└{'─'*91}┘")
-
     print(currentPlayerPosition)
 
+#potrzebne zmienne
 
 currentPlayer = 1
 currentPlayerSign = "O"
+wylosowane = 0
+wybor = 0
+infoText = ""
 
 def switchPlayer():
+    #przełączanie gracza
     global currentPlayer
     global currentPlayerSign
 
@@ -137,44 +143,155 @@ def switchPlayer():
         currentPlayer = 1
         currentPlayerSign = "O"
 
-
 def dice():
+    #losowanie ilości pól na 2 kostki
     return [random.randint(1,6), random.randint(1,6)]
         
-wylosowane = 0
-wybor = 0
+def chance():
+    #losowanie od 1 do 50000 potrzebne do dodawania pieniędzy na polu szansa
+    return random.randint(1, 50000)
+
+def info():
+    #funkcja do wyświetlania komunikatów
+    if infoText != "":
+        print(infoText)
+
+def taxField():
+    #podatki
+    global infoText
+    tabelaGraczy[currentPlayer-1].stan_konta -= tabelaGraczy[currentPlayer-1].stan_konta*0.1
+    infoText = "Pobrano podatek"
+
+def getMoney():
+    #dodanie graczowi stającemu na polu impreza po 50k od każdego gracza
+    global infoText
+    for i in tabelaGraczy:
+        i.stan_konta -= 50000
+    
+    tabelaGraczy[currentPlayer-1].stan_konta += 200000
+    infoText = "Dostałeś od każdego gracza 50k"
+
+def protectedField():
+    #cofnięcie o randomową ilość pól gracza który stanął na polu strzeżonym
+    global infoText
+    randomNb = random.randint(1, 12)
+    currentPlayerPosition[currentPlayer-1] -= randomNb
+    infoText = f"Trafiłeś na pole strzeżone - zostałeś cofnięty o {randomNb} pól"
+
+def jail():
+    #przeniesienie z więzienia na start
+    global infoText
+    tablicaPol[currentPlayerPosition[currentPlayer-1]].gracz[currentPlayer-1] = " "
+    currentPlayerPosition[currentPlayer-1] = 0
+    infoText = "Stanąłeś na polu więzienie - zostałeś deportowany na pole start"
+
+def checkWin():
+    a = 0
+    for i in tabelaGraczy[currentPlayer-1].posiadlosci:
+        #sprawdzanie kurortów
+        if i == 'Cypr' or i == 'Nicea' or i == 'Dubaj' or i == 'Bali':
+            a += 1
+
+        if a == 4:
+            os.system('cls')
+            print(f"Wygrał gracz {currentPlayerSign}")
+            sys.exit(0)
+
+        #sprawdzanie monopoli
+
+    help = [0, 0, 0, 0, 0, 0, 0, 0]
+    monopole = [0, 0, 0, 0, 0, 0, 0, 0]
+    o = 0
+
+    for i in tabelaGraczy[currentPlayer-1].posiadlosci:
+        if i == 'Grenada' or i == 'Sewila' or i == 'Madryt':
+            help[0] += 1
+        elif i == 'Hongkong' or i == 'Pekin' or i == 'Szanghaj':
+            help[1] += 1
+        elif i == 'Wenecja' or i == 'Mediolan' or i == 'Rzym':
+            help[2] += 1
+        elif i == 'Hamburg' or i == 'Berlin':
+            help[3] += 1
+        elif i == 'Londyn' or i == 'Sydnay':
+            help[4] += 1
+        elif i == 'Chicago' or i == 'LasVegas' or i == 'NowyJork':
+            help[5] += 1
+        elif i == 'Lyon' or i == 'Paryz':
+            help[6] += 1
+        elif i == 'Krakow' or i == 'Warszawa':
+            help[7] += 1 
+
+        if help[0] == 3:
+            monopole[0] = 1
+        elif help[1] == 3:
+            monopole[1] = 1
+        elif help[2] == 3:
+            monopole[1] = 1
+        elif help[3] == 2:
+            monopole[1] = 1
+        elif help[4] == 2:
+            monopole[1] = 1
+        elif help[5] == 3:
+            monopole[1] = 1
+        elif help[6] == 2:
+            monopole[1] = 1
+        elif help[7] == 2:
+            monopole[1] = 1
+        
+        for j in monopole:
+            o += j
+            if o == 1:
+                os.system('cls')
+                print(f"Wygrał gracz {currentPlayerSign}")
+                sys.exit(0)
 
 def movePlayer():
+    #poruszanie się pionkami i sprawdzanie pól
     global currentPlayer
     global currentPlayerSign
+    global infoText
 
-    tablicaPol[currentPlayerPosition[currentPlayer-1]].gracz[currentPlayer-1] = " "
+    if tabelaGraczy[currentPlayer-1].stan_konta > -1:
 
-    currentPlayerPosition[currentPlayer-1] += wylosowane[0]+wylosowane[1]
+        #zerowanie poprzedniego pola gracza
 
-    if currentPlayerPosition[currentPlayer-1] >= 32:
-        currentPlayerPosition[currentPlayer-1] -= 32
-        tabelaGraczy[currentPlayer-1].stan_konta += 400000
-        tablicaPol[currentPlayerPosition[currentPlayer-1]].gracz[currentPlayer-1] = currentPlayerSign
+        tablicaPol[currentPlayerPosition[currentPlayer-1]].gracz[currentPlayer-1] = " "
+        currentPlayerPosition[currentPlayer-1] += wylosowane[0]+wylosowane[1]
 
+        #pola specjalne - wywołanie funkcji
+
+        if currentPlayerPosition[currentPlayer-1] == 12 or currentPlayerPosition[currentPlayer-1] == 20 or currentPlayerPosition[currentPlayer-1] == 28:
+            tabelaGraczy[currentPlayer-1].stan_konta += chance()
+        elif currentPlayerPosition[currentPlayer-1] == 8:
+            jail()
+        elif currentPlayerPosition[currentPlayer-1] == 16:
+            getMoney() 
+        elif currentPlayerPosition[currentPlayer-1] == 24:
+            protectedField()
+        elif currentPlayerPosition[currentPlayer-1] == 30:
+            taxField()
+
+        #skok z przez start
+
+        if currentPlayerPosition[currentPlayer-1] > 31:
+            currentPlayerPosition[currentPlayer-1] -= 32
+            tabelaGraczy[currentPlayer-1].stan_konta += 100000
+            tablicaPol[currentPlayerPosition[currentPlayer-1]].gracz[currentPlayer-1] = currentPlayerSign
+        else:
+            tablicaPol[currentPlayerPosition[currentPlayer-1]].gracz[currentPlayer-1] = currentPlayerSign
     else:
-        tablicaPol[currentPlayerPosition[currentPlayer-1]].gracz[currentPlayer-1] = currentPlayerSign
+        infoText = "Jesteś bankrutem - nie grasz"
+        currentPlayerPosition[currentPlayer-1] = 0
 
 def tax():
+    #podatek od stanięcia na polu innego gracza
     if tablicaPol[currentPlayerPosition[currentPlayer-1]].zakupiono == 1:
             tabelaGraczy[currentPlayer-1].stan_konta -= tablicaPol[currentPlayerPosition[currentPlayer-1]].podatek
-            tabelaGraczy[tablicaPol[currentPlayerPosition[currentPlayer-1]].wlasciciel-1].stan_konta += tablicaPol[currentPlayerPosition[currentPlayer-1]].podatek #chyba działa, przetestować
+            tabelaGraczy[tablicaPol[currentPlayerPosition[currentPlayer-1]].wlasciciel-1].stan_konta += tablicaPol[currentPlayerPosition[currentPlayer-1]].podatek
 
-            print("Pobrano podatek na rzecz właściciela.")
-
-
-
-
-
-    
-    
 def buyField():
-    if currentPlayerPosition[currentPlayer-1] != 0 and currentPlayerPosition[currentPlayer-1] != 8 and currentPlayerPosition[currentPlayer-1] != 16 and tablicaPol[currentPlayerPosition[currentPlayer-1]].zakupiono == 0:
+    #kupowanie pola
+    if currentPlayerPosition[currentPlayer-1] != 0 and currentPlayerPosition[currentPlayer-1] != 8 and currentPlayerPosition[currentPlayer-1] != 16 and currentPlayerPosition[currentPlayer-1] != 12 and currentPlayerPosition[currentPlayer-1] != 20 and currentPlayerPosition[currentPlayer-1] != 24 and currentPlayerPosition[currentPlayer-1] != 28 and currentPlayerPosition[currentPlayer-1] != 30 and tablicaPol[currentPlayerPosition[currentPlayer-1]].zakupiono == 0 and tabelaGraczy[currentPlayer-1].stan_konta > tablicaPol[currentPlayerPosition[currentPlayer-1]].cena:
         tabelaGraczy[currentPlayer-1].stan_konta -= tablicaPol[currentPlayerPosition[currentPlayer-1]].cena
         tabelaGraczy[currentPlayer-1].posiadlosci.append(tablicaPol[currentPlayerPosition[currentPlayer-1]].nazwa)
         print(f"Kupiono pole {tablicaPol[currentPlayerPosition[currentPlayer-1]].nazwa}")
@@ -186,25 +303,27 @@ def buyField():
     else:
         print("Nie można kupić tego pola")
 
-
-
+#główna pętla gry, wywoływanie funkcji i wypisywanie informacji
 
 while True:
     setBoard() 
     
-    if int(wybor) == 1:
+    if wybor == '1':
         wybor = 0
         wylosowane = dice()
-
 
         movePlayer()
         setBoard() 
         tax()
+        
 
         print(f"Wylosowane liczby to: {wylosowane[0]} i {wylosowane[1]}")
+
+        info()
+        infoText = ""
         
-        print(f"Gracz: {currentPlayerSign} | Stan konta: {tabelaGraczy[currentPlayer-1].stan_konta} | Pole: {tablicaPol[currentPlayerPosition[currentPlayer-1]].nazwa} | Cena: {tablicaPol[currentPlayerPosition[currentPlayer-1]].cena} | Rola: {tablicaPol[currentPlayerPosition[currentPlayer-1]].rola}")
-        
+        print("---------------")
+        print(f"Gracz: {currentPlayerSign} | Stan konta: {tabelaGraczy[currentPlayer-1].stan_konta} | Pole: {tablicaPol[currentPlayerPosition[currentPlayer-1]].nazwa} | Cena: {tablicaPol[currentPlayerPosition[currentPlayer-1]].cena} | Rola: {tablicaPol[currentPlayerPosition[currentPlayer-1]].rola}")      
         print("---------------")
         print(f"Stan konta 1 gracza: {tabelaGraczy[0].stan_konta}")
         print(f"Stan konta 2 gracza: {tabelaGraczy[1].stan_konta}")
@@ -220,25 +339,25 @@ while True:
         
         print("Co chcesz zrobić?")
         print("1 - kup pole")
-        print("2 - zakończ rundę")
+        print("reszta - zakończ rundę")
 
         
         wybor2 = input("Wybór: ")
 
-        if int(wybor2) == 1:
+        if wybor2 == '1':
             buyField()
+            checkWin()
 
         switchPlayer()
+        
 
 
-        
-    else:
-        print("Niepoprawna opcja")
-        
-    
+    print(" ")  
+    print("1 - rzut kostką")
     print("Co chcesz zrobić?")
     
     while wybor != "1":
+        
         wybor = input("Wybór: ")
 
     
